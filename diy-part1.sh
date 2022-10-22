b8df1590a28668590553c65a68d48ca297db90d8
Mark <55911453+ufk119@users.noreply.github.com>
2022-10-22 23:57:33 +0800

    Update diy-part1.sh
    添加自己喜欢的控件

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,16 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+
+
+echo 'src-git nas      https://github.com/linkease/nas-packages.git' >>feeds.conf.default
+echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git' >>feeds.conf.default
+
+
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+
+echo 'src-git kiddin9_pkg https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
+
+
